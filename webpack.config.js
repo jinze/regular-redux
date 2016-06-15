bb0c708b1483e17e2d8982dfe1207326fd5540e2
Jin Ze <luckyruisin@gmail.com>
2016-06-15 16:02:29 +0800

    config mistake

--- webpack.config.js
+++ webpack.config.js
@@ -4,34 +4,41 @@ var webpack = require('webpack')
 var env = process.env.NODE_ENV
 
 var webpackConfig = {
-    externals: {
-        "regularjs": "Regular",
-        'redux': "Redux"
-    },
-    module: {
-        loaders: [
-            { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
-        ]
-    },
-    output: {
-        library: 'RegularRedux',
-        libraryTarget: "umd"
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(env)
+  externals: {
+    "regularjs": "Regular",
+    'redux': "Redux"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
+    ]
+  },
+  output: {
+    library: 'RegularRedux',
+    libraryTarget: "umd"
+  },
+  plugins: [
+    {
+      apply: function apply(compiler) {
+        compiler.parser.plugin('expression global', function expressionGlobalPlugin() {
+          this.state.module.addVariable('global', "(function() { return this; }()) || Function('return this')()")
+          return false
         })
-    ],
-    devtool: "source-map",
-    watch: true
+      }
+    },
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(env)
+    })
+  ]
 };
 
 if(env === 'production'){
-    webpackConfig.plugins.push([
-        new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false }
-        })
-    ])
+  webpackConfig.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    })
+  )
 }
 
 
