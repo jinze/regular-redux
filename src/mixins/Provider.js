f9e2896400a40ab737628f35ced4fafc0c8a69e5
Jin Ze <luckyruisin@gmail.com>
2016-06-18 09:14:28 +0800

    provider

--- src/mixins/Provider.js
+++ src/mixins/Provider.js
@@ -1,14 +1,19 @@
 let Provider = Component =>
   Component.implement({
     events: {
-      $config: () =>
+      $config: function(){
         this.__reduxStore = this.store
+
         // check if it is the only store in context
-        // let storeCheck = component =>
-        //   if(component)
-        //   if(component.$parent){
-        //     return storeCheck(component.$parent)
-        //   }
+        let parentNode = this.$parent
+        while(parentNode){
+          if(parentNode.__reduxStore){
+            throw new Error('More than one store in Provider context')
+          }
+          parentNode = parentNode.$parent
+        }
+
+      }
 
     }
   })
